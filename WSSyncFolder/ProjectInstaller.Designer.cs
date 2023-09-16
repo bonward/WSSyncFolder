cbd16af1085b88b8fcc737ece4442636560f22aa
B-VM-DEV-XI\Moreno <moreno.beninca@bonward.com>
2023-09-16 10:02:43 +0200

    Primo commit: servizio funzionante con impostazioni di test

---
+++ WSSyncFolder/ProjectInstaller.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WSSyncFolder
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            //
+            // Add by Moreno
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            //
+            // Add by Moreno
+            this.serviceInstaller1.Description = "B Onward WSSyncFolder Sincronyze Folders Contents with configurable options";
+            this.serviceInstaller1.DisplayName = "B Onward WSSyncFolder";
+
+            this.serviceInstaller1.ServiceName = "WSSyncFolder-Service1";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ WSSyncFolder/README.md
@@ -0,0 +1,27 @@
+# WSSyncFolder
+
+## Create a Windows Service in C#
+
+[Reference](https://www.c-sharpcorner.com/article/create-windows-services-in-c-sharp/)
+
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319>
+
+### Installal il servizio
+
+* InstallUtil.exe -u C:\Users\Moreno\source\repos\WSSyncFolder\WSSyncFolder\bin\Debug\WSSyncFolder.exe
+
+### Disinstalla il servizio
+
+* InstallUtil.exe -u C:\Users\Moreno\source\repos\WSSyncFolder\WSSyncFolder\bin\Debug\WSSyncFolder.exe
+
+## Move all files from one folder to another in C#
+
+[Reference](https://www.techiedelight.com/move-all-files-from-one-folder-to-another-csharp/)
+
+## Understanding Enums in C# & it's advantages with example
+
+[Reference](https://qawithexperts.com/article/c-/understanding-enums-in-c-its-advantages-with-example/90)
+
+## How to read a JSON file in C#
+
+[Reference](https://www.educative.io/answers/how-to-read-a-json-file-in-c-sharp)
